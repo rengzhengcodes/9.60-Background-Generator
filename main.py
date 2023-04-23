952926f5f41cc479301cc9d1229486baab78aa1e
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-04-23 00:43:13 -0400

    finished end

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ def generate_blocks_and_offsets(
     ret: dict[tuple[int, int], tuple[tuple[int, int]]] = {}
 
     # generates the blocks assuming a square
-    for row_start in range(0, div_size, size):
-        for col_start in range(0, div_size, size):
+    for row_start in range(0, size, div_size):
+        for col_start in range(0, size, div_size):
             # initializes the list
             ret[(row_start, col_start)] = []
 
@@ -85,8 +85,40 @@ def generate_background(size: int = 256, seed: int = 42, difficulty: int = 0):
     Returns:
         A background of difficulty (2^difficulty differed tiles).
     """
+    # guarantees a new image canvas and entropy
     canvas: Image
     brush: Random
     canvas, brush = get_canvas_and_brush((size, size), seed)
 
+    # gets the block offsets for this specific image
+    blocks: dict = generate_blocks_and_offsets(
+        size, difficulty
+    )
+
+    # block starts
+    starts: tuple[tuple[int, int]] = tuple(blocks.keys())
+    # block offsets
+    offs: tuple[tuple[tuple[int, int]]] = tuple(blocks.values())
+
+    # goes through all the starts
+    index: int
+    start: tuple[int, int]
+    for index, start in enumerate(starts):
+        # gets the start coords
+        x_start: int
+        y_start: int
+        x_start, y_start = start
+        # generates the color for the coord
+        color: tuple = get_random_color(brush)
+
+        # goes through the offsets for this start
+        x_off: int
+        y_off: int
+        for x_off, y_off in offs[index]:
+            canvas.putpixel((x_start + x_off, y_start + y_off), color)
+
     return canvas
+
+if __name__ == "__main__":
+    for i in range(9):
+        generate_background(difficulty=i).save(f"difficulty{i}.png", "PNG")
