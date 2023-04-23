8b66e17ec30b5fa57089e3ddd15d0eba54539540
Renggeng Zheng <rzhengcontacts@gmail.com>
2023-04-22 23:12:18 -0400

    colors works

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 """The image generation algorithm"""
 
-import PIL
+# random library for random background color fixing
+from random import Random
 
+# image editing software for generation
+from PIL import Image
+
+
+def get_random_color(rng: Random) -> tuple:
+    """Generates a random RGB color tuple
+
+    Attributes:
+        rng: The rng source.
+    
+    Returns:
+        A tuple of form (r, g, b)
+    """
+
+    return (
+            rng.randint(0, 255),
+            rng.randint(0, 255),
+            rng.randint(0, 255)
+        )
+
+
+def get_canvas_and_brush(
+    size: tuple = (256, 256), seed: int = 42
+) -> tuple[Image, Random]:
+    """
+    """
+
+if __name__ == "__main__":
+    entropy: Random = Random(42)
+
+    # ensures no 
+    print(get_random_color(entropy))
